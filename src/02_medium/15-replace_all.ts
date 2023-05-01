66d9957c6d809b1b30a36771f656c16a3707246a
kitamuraDev <masato1999824@icloud.com>
2023-05-01 21:57:28 +0900

    :adhesive_bandage: 回答のサンプルがまったく同じだったため、片方を修正

--- src/02_medium/15-replace_all.ts
+++ src/02_medium/15-replace_all.ts
@@ -8,8 +8,8 @@
 
 // prettier-ignore
 type ReplaceAll< S extends string, From extends string, To extends string >
-  = From extends "" ? S
-  : S extends `${infer L}${From}${infer R}` ? `${L}${To}${ReplaceAll<R, From, To>}`
+  = From extends '' ? S
+  : S extends `${infer L}${From}${infer R}` ? `${ReplaceAll<L, From, To>}${To}${ReplaceAll<R, From, To>}`
   : S;
 
 // R に対してのみ再帰処理をかけても結果は同じ
