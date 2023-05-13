dd7d9a2964db54f79af2dc22b3b31e951ebf5779
kitamuraDev <masato1999824@icloud.com>
2023-05-13 23:01:17 +0900

    :sparkles: medium / reverse

---
+++ src/02_medium/44-reverse.ts
@@ -0,0 +1,20 @@
+/* _____________ Your Code Here _____________ */
+
+/**
+ * 1. タプル型を先頭をそれ以外に分ける
+ * 2. F が後ろに来るように新しい配列を作成する（Reverse、つまり R は ... で展開する）
+ * 3. T が空の配列であれば、Tを返す
+ */
+type Reverse<T extends readonly unknown[]> = T extends [infer F, ...infer R]
+  ? [...Reverse<R>, F]
+  : T;
+
+/* _____________ Test Cases _____________ */
+
+import type { Equal, Expect } from "../utils/index.d.ts";
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<["a", "b"]>, ["b", "a"]>>,
+  Expect<Equal<Reverse<["a", "b", "c"]>, ["c", "b", "a"]>>
+];
